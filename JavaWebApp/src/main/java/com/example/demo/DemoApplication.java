91041eeb3ccd91e7ba25b843df26aeeecbe99357
mbawillis@yahoo.com <mbawillis@yaho.com>
2022-10-18 18:36:07 -0500

    modified app code

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,5 +9,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+        String name = "willis"
+		System.out.println(name)
 }
